550212f39885f1b5f105ec3f9197591f51ffe342
PandaDeveloper13 <9025264@student.zadkine.nl>
2025-09-15 15:57:55 +0200

    Added shop and autoclicker

--- Cookies_Clicker.js
+++ Cookies_Clicker.js
@@ -1,10 +1,32 @@
 let count = 0;
 const countDiv = document.getElementById('count');
 const btn = document.getElementById('cookieBtn');
+const shopBtn = document.getElementById('shopBtn');
+const AutoRate = 1000; // elke seconde
+let autoClickerActive = false; // zodat hij maar 1 keer start
 
+// Cookie button
 btn.addEventListener('click', function() {
     count++;
     countDiv.textContent = count;
     btn.classList.add('clicked');
     setTimeout(() => btn.classList.remove('clicked'), 200);
 });
+
+// Shop button
+shopBtn.addEventListener('click', function() {
+    if (count >= 20 && !autoClickerActive) {
+    count -= 20; // kosten voor autoclicker
+    countDiv.textContent = count;
+    autoClickerActive = true;
+
+    setInterval(() => {
+    count++;
+    countDiv.textContent = count;
+}, AutoRate);
+} else if (count < 20) {
+    alert("Je hebt minimaal 20 cookies nodig!");
+}
+});
+
+
